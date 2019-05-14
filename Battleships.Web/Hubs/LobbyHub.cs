cd13114044cb114ebb8d53d13203c3cdea85e4e5
Bartosz.C <bartosz.ciesielski1997@gmail.com>
2019-05-14 11:24:25 +0200

    Added connectionmapping to keep track of connecter users. Added online status setting on log in

---
+++ Battleships.Web/Hubs/LobbyHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Battleships.Web
+{
+	public class LobbyHub : Hub
+	{
+		private readonly static ConnectionMapping<string> _connections =
+			new ConnectionMapping<string>();
+
+
+		public override Task OnConnected()
+		{
+			string name = Context.User.Identity.Name;
+			_connections.Add(name, Context.ConnectionId);
+			Groups.Add(Context.ConnectionId, name);
+			Clients.Others.LogInUser(name);
+
+			return base.OnConnected();
+		}
+
+		public void Send(string name, string message)
+		{
+			// Call the addNewMessageToPage method to update clients.
+			Clients.All.addNewMessageToPage(name, message);
+		}
+
+		public void Feedback(string name,string caller)
+		{
+			Clients.Group(caller).LogInUser(name);
+		}
+
+
+
+	}
+}
